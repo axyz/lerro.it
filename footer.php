40769f50e1a886d5a68540ea05a5cc28b7a33aa1
Andrea Moretti <axyzxp@gmail.com>
2012-12-27 12:11:58 +0100

    -

--- footer.php
+++ footer.php
@@ -3,17 +3,14 @@
     <div class="row">
 
       <div class="six columns">
-          <p>&copy; Copyright Andrea Moretti 2012.</p>
+          <p>&copy; Copyright lerro.it 2012.</p>
       </div>
 
       <div class="six columns">
         <ul class="link-list right">
           <li><a href="#">Home</a></li>
-          <li><a href="#">Photography</a></li>
-          <li><a href="#">Web Design</a></li>
-          <li><a href="#">Coding</a></li>
-          <li><a href="#">Writing</a></li>
           <li><a href="#"><i class="icon-info-sign"></i>About</a></li>
+          <li><a href="<?php echo $basedir;?>contact.php">Contact</a></li>
         </ul>
       </div>
 
--- nav.php
+++ nav.php
@@ -5,10 +5,6 @@
     </ul>
     <section>
       <ul class="right">
-        <li><a href="<?php echo $basedir;?>photography.php">Photography</a></li>
-        <li><a href="<?php echo $basedir;?>web-design.php">Web Design</a></li>
-        <li><a href="#">Coding</a></li>
-        <li><a href="#">Writing</a></li>
         <li><a href="#"><i class="icon-info-sign"></i>About</a></li>
         <li><a href="<?php echo $basedir;?>contact.php">Contact</a></li>
       </ul>
