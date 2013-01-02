ff49c03df201665280fa665b6faaaf2a775f1b5f
MCM <MCM@server.local>
2013-01-02 14:58:34 +0100

    -

--- index.php
+++ index.php
@@ -4,10 +4,6 @@
 <?php include("nav.php"); ?><br/><br/>
 <div id="maximage"><img src="http://placehold.it/2560x600&amp;text=BG1"/><img src="http://placehold.it/2560x600&amp;text=BG2"/><img src="http://placehold.it/2560x600&amp;text=BG3"/></div>
 <div id="content" class="row">
-  <div class="twelve columns">
-    <h1>Title</h1>
-    <p><span>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis vitae culpa sit nobis alias inventore perferendis id voluptatibus labore modi dignissimos deleniti est asperiores consectetur eveniet veniam architecto et porro."</span><span>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis vitae culpa sit nobis alias inventore perferendis id voluptatibus labore modi dignissimos deleniti est asperiores consectetur eveniet veniam architecto et porro."</span><span>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis vitae culpa sit nobis alias inventore perferendis id voluptatibus labore modi dignissimos deleniti est asperiores consectetur eveniet veniam architecto et porro."</span><span>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis vitae culpa sit nobis alias inventore perferendis id voluptatibus labore modi dignissimos deleniti est asperiores consectetur eveniet veniam architecto et porro."</span><span>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis vitae culpa sit nobis alias inventore perferendis id voluptatibus labore modi dignissimos deleniti est asperiores consectetur eveniet veniam architecto et porro."</span>
-    </p>
-  </div>
+  <div class="twelve columns"></div>
 </div><?php include("footer.php"); ?>
 <?php include("tail.php"); ?>
